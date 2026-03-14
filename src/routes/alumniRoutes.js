b8c08bc69938a876f99685a55312f476d0b51867
kebabresing <holainc115@gmail.com>
2026-03-14 09:51:32 +0700

    feat: Alumni Tracker - sistem pelacakan alumni UMM dengan bot simulasi dan dashboard statistik

--- src/routes/alumniRoutes.js
+++ src/routes/alumniRoutes.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 const alumniController = require('../controllers/alumniController');
 
-router.get('/', alumniController.index);
+router.get('/', alumniController.getLaporan);
+router.get('/data', alumniController.index);
 router.get('/add', alumniController.formAdd);
 router.post('/add', alumniController.add);
 router.get('/edit/:id', alumniController.formEdit);
 router.post('/edit/:id', alumniController.edit);
 router.get('/delete/:id', alumniController.delete);
 router.get('/simulate', alumniController.simulateBot);
-router.get('/laporan', alumniController.getLaporan);
 
 module.exports = router;
